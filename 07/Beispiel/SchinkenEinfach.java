cf39bb9b65f0abce2f914d3cca2374ce4a32bb05
Konstantin Zangerle <info@konstantinzangerle.de>
2015-12-19 22:05:53 +0100

    Schinken einfach

--- 07/Beispiel/SchinkenEinfach.java
+++ 07/Beispiel/SchinkenEinfach.java
@@ -1,16 +1,16 @@
 import java.util.Arrays;
 
-public class Schinken implements Comparable<Schinken>{
+public class SchinkenEinfach implements Comparable<SchinkenEinfach>{
     private int size;
     private int weight;
     
-    Schinken(int s, int w) {
+    SchinkenEinfach(int s, int w) {
         size = s;
         weight = w;
     }
     
     @Override
-    public int compareTo(Schinken another) {
+    public int compareTo(SchinkenEinfach another) {
         if (this.size < another.size){
             return -1;
         } else if (this.size == another.size) {
@@ -28,19 +28,15 @@ public class Schinken implements Comparable<Schinken>{
     
     
     public static void main(String[] args) {
-        Schinken hans = new Schinken(3,5);
-        Schinken herbert = new Schinken(24,78);
+        SchinkenEinfach hans = new SchinkenEinfach(3,5);
+        SchinkenEinfach herbert = new SchinkenEinfach(24,78);
         
         System.out.println(hans.compareTo(herbert));
         
-/*        
-        if (hans < herbert) {
-            System.out.println("ente");
-        }
-*/
-        Schinken[] arr = {hans, herbert, new Schinken(32,2)};
+ 
+        SchinkenEinfach[] arr = {hans, herbert, new SchinkenEinfach(32,2)};
         Arrays.sort(arr);
-        for (Schinken i : arr) {
+        for (SchinkenEinfach i : arr) {
             System.out.println(i.size);
         }
 
